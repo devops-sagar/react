e7a2e1c0ab3ae162f56ed6f155096524f7a156f1
devops-sagar <pathaksagar16@gmail.com>
2023-09-10 23:10:18 -0400

    updated components

---
+++ yt/portfolio/src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">Sagar</a>
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
